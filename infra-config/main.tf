10f56b46ad26c544e15dbd59753ff44f3ba13c2d
Riley <rileyp.mckinley@gmail.com>
2025-06-28 17:16:42 -0400

    #2

---
+++ infra-config/main.tf
@@ -0,0 +1,229 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "lab_vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "main"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.lab_vpc.id
+}
+
+resource "aws_route_table" "lab_routes" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "lab"
+  }
+}
+
+resource "aws_subnet" "lab_private_a" {
+    vpc_id = aws_vpc.lab_vpc.id
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.0.0/24"
+    
+    tags = {
+      Name = "lab-private-a"
+    }
+}
+
+resource "aws_subnet" "lab_private_b" {
+    vpc_id = aws_vpc.lab_vpc.id
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      Name = "lab-private-b"
+    }
+}
+
+resource "aws_subnet" "lab_public_a" {
+    vpc_id = aws_vpc.lab_vpc.id
+    availability_zone = "us-east-1a"
+    cidr_block = "10.0.2.0/24"
+    tags = {
+      Name = "lab-public-a"
+    }
+}
+
+resource "aws_subnet" "lab_public_b" {
+    vpc_id = aws_vpc.lab_vpc.id
+    availability_zone = "us-east-1b"
+    cidr_block = "10.0.3.0/24"
+    tags = {
+      Name = "lab-public-b"
+    }
+}
+
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.main.id
+#   route_table_id = aws_route_table.lab_routes.id
+# }
+
+resource "aws_security_group" "lab_lb_sg" {
+  description = "lab security group"
+  vpc_id      = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "lab-lb-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-hhtps" {
+  security_group_id = aws_security_group.lab_lb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol          = "tcp"
+  tags = {
+    Name = "allow-http"
+  }
+}
+
+#resource "aws_vpc_security_group_ingress_rule" "allow-ssh" {
+#  security_group_id = aws_security_group.lab-sg.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  from_port         = 22
+#  to_port           = 22
+#  ip_protocol       = "tcp"
+#  tags = {
+#    Name = "allow-ssh"
+#  }
+#}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+ security_group_id = aws_security_group.lab_lb_sg.id
+ cidr_ipv4         = "0.0.0.0/0"
+ ip_protocol       = "-1"
+}
+
+resource "aws_launch_template" "lab_template" {
+  name = "lab-webserver-template"
+  image_id           = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "lab-web-ec2"
+  vpc_security_group_ids = [aws_security_group.lab_lb_sg.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  
+}
+
+resource aws_autoscaling_group "lab_asg" {
+  launch_template {
+    id = aws_launch_template.lab_template.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = [aws_subnet.lab_private_a.id, aws_subnet.lab_private_b.id]
+  target_group_arns = [aws_lb_target_group.lab_lb_tg.arn]
+  health_check_type = "ELB"
+
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key = "Name"
+    value = "WEB"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "cpu-target-tracking"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.lab_asg.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0  # Maintain avg CPU at 50%
+  }
+}
+
+
+resource aws_lb "lab_lb" {
+  name = "lab-lb"
+  load_balancer_type = "application"
+  subnets = [aws_subnet.lab_private_a.id, aws_subnet.lab_private_b.id]
+  security_groups = [aws_security_group.lab_lb_sg.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.lab_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "http-rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lab_lb_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "lab_lb_tg" {
+  name     = "lab-lb-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.lab_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+output "lb_dns_name" {
+  value       = aws_lb.lab_lb.dns_name
+  description = "The domain name of the load balancer"
+}
+
+# terraform {
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket         = "rileys-terraform-up-and-running-state"
+#     key            = "global/s3/terraform.tfstate"
+#     region         = "us-east-1"
+
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "terraform-up-and-running-locks"
+#     encrypt        = true
+#   }
+# }
